67db76b8205057ae9b56373faa85cde33a061091
Leonardo Setiadi <setiadileonardo@gmail.com>
2020-10-05 12:10:26 +0700

    First commit

---
+++ QrScanner/src/main/java/com/deeeplabs/qrscanner/QrScannerActivity.kt
@@ -0,0 +1,40 @@
+package com.deeeplabs.qrscanner
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.Result
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+
+class QrScannerActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
+    private var mScannerView: ZXingScannerView? = null
+
+    public override fun onCreate(state: Bundle?) {
+        super.onCreate(state)
+        mScannerView = ZXingScannerView(this)   // Programmatically initialize the scanner view
+        setContentView(mScannerView)                // Set the scanner view as the content view
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        mScannerView!!.setResultHandler(this) // Register ourselves as a handler for scan results.
+        mScannerView!!.startCamera()          // Start camera on resume
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mScannerView!!.stopCamera()           // Stop camera on pause
+    }
+
+    override fun handleResult(result: Result) {
+        val resultIntent = Intent()
+        resultIntent.putExtra("Result", result.text)
+
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
+
+        // If you would like to resume scanning, call this method below:
+        //mScannerView.resumeCameraPreview(this);
+    }
+}
